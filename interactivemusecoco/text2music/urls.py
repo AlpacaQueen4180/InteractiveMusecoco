f2747dbb8426b36d84cf62e300c82ee445ff881e
alpaca <30415a@gmail.com>
2024-01-20 12:03:30 +0800

    add actual generation

--- interactivemusecoco/text2music/urls.py
+++ interactivemusecoco/text2music/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path("download_midi/", views.download_midi, name="download_midi"),
 ]
